29b039686a17261518b7d9c541a303b911e93f71
Nathan Woltman <nwoltman@outlook.com>
2014-12-01 18:07:09 -0500

    Formatted Parent similar to other files

--- parent.cc
+++ parent.cc
@@ -1,25 +1,26 @@
 #include "parent.h"
 #include "mprng.h"
 
-Parent::Parent( Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay ) : _prt(prt), _bank(bank), _numStudents(numStudents), _parentalDelay(parentalDelay) {
-}
+Parent::Parent( Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay )
+    : _prt(prt), _bank(bank), _numStudents(numStudents), _parentalDelay(parentalDelay) { }
+
+Parent::~Parent() { }
 
 void Parent::main() {
-	_prt.print(Printer::Parent, 'S');
-	
-	while (true) {
-		
+	_prt.print( Printer::Parent, (char)Parent::Starting ); // Starting task
+
+	for ( ;; ) {
 		_Accept(~Parent) break;
 		_Else {}
-		
-		yield(_parentalDelay); // delay parentalDelay times
-		
-		unsigned int randomMoney = g_mprng(1, 3);
-		unsigned int randomStudent = g_mprng(_numStudents);
-		
-		_bank.deposit(randomStudent, randomMoney);
-		_prt.print(Printer::Parent, 'D', randomStudent, randomMoney);
+
+		yield( _parentalDelay ); // delay parentalDelay times
+
+		unsigned int randomMoney = g_mprng( 1, 3 );
+		unsigned int randomStudent = g_mprng( _numStudents );
+
+		_bank.deposit( randomStudent, randomMoney );
+		_prt.print( Printer::Parent, (char)Parent::Deposit, randomStudent, randomMoney );
 	}
-	
-	_prt.print(Printer::Parent, 'F');
+
+	_prt.print( Printer::Parent, (char)Parent::Finished ); // Finished task
 }
--- parent.h
+++ parent.h
@@ -5,15 +5,15 @@
 #include "bank.h"
 
 _Task Parent {
-	Printer &_prt;
-	Bank &_bank;
-	unsigned int _numStudents;
-	unsigned int _parentalDelay;
-	
-	~Parent() {};
+    enum States { Starting = 'S', Deposit = 'D', Finished = 'F' };
+    Printer &_prt;
+    Bank &_bank;
+    unsigned int _numStudents;
+    unsigned int _parentalDelay;
     void main();
   public:
     Parent( Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay );
+    ~Parent();
 };
 
 #endif
