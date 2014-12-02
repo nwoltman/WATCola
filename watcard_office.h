764fe301bae7e4bdb44b994f8918a1a73b70bf0f
Nathan Woltman <nwoltman@outlook.com>
2014-12-01 19:29:35 -0500

    Fixed syntax errors in watcard_office.h
    Also did some formatting

--- watcard_office.h
+++ watcard_office.h
@@ -7,34 +7,35 @@
 #include <queue>
 
 _Task WATCardOffice {
-	struct Args {
-		unsigned int _sid,
-		unsigned int _amount;
-		WATCard *_card;
-		Args(unsigned int sid, unsigned int amount, WATCard::FWATCard &card) : _sid(sid), _amount(amount), _card(card) {}
-	}
-	
+    struct Args {
+        unsigned int _sid;
+        unsigned int _amount;
+        WATCard *_card;
+        Args( unsigned int sid, unsigned int amount, WATCard::FWATCard &card )
+            : _sid( sid ), _amount( amount ), _card( card ) {}
+    };
+
     struct Job {                           // marshalled arguments and return future
-        Args args;                         // call arguments (YOU DEFINE "Args")
+        Args args;                         // call arguments
         WATCard::FWATCard result;          // return future
         Job( Args args ) : args( args ) {}
     };
+
     _Task Courier { // communicates with bank
 		unsigned int _id;
 		Printer &_prt;
 		Bank &_bank;
 		WATCardOffice &_watOffice;
 		void main();
-		
+
 		enum State {
 			Starting = 'S', TransferStart = 't', TransferComplete = 'T', Finished = 'F'
 		};
 	  public:
 		Courier( unsigned int id, Printer &prt, Bank &bank, WATCardOffice &watOffice );
-	};                 
-	
+	};
+
 	Courier *_couriers;
-	std::vector<Courier*> _couriers;
 	std::queue<Job*> _jobs;
 	Job _currentJob;
 	Printer &_prt;
@@ -42,11 +43,11 @@ _Task WATCardOffice {
 	unsigned int _numCouriers;
 	uCondition courierBench;
     void main();
-    
+
     enum State {
         Starting = 'S', RequestComplete = 'W', CreateComplete = 'C', TransferComplete = 'T', Finished = 'F'
     };
-    
+
   public:
     _Event Lost {};                        // lost WATCard
     WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers );
